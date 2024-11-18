91cb2ee55b5e1d8e47ccd1ff205f78340e37320c
Pete McCarthy <petefromsf@gmail.com>
2024-11-17 22:09:41 -0700

    Embed page

---
+++ src/pages/Embed.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './Embed.scss';
+
+export const EmbedPage: React.FC = () => {
+  return (
+    <div className="embed-page">
+      <h1>Embed Page</h1>
+      {/* Add your embed-specific content here */}
+    </div>
+  );
+};
+
+export default EmbedPage;
