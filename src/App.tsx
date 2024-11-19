a42cfedab82d7d3d333ae8d4730d6ec21692d543
Pete McCarthy <petefromsf@gmail.com>
2024-11-18 18:16:17 -0700

    Good starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ConsolePage } from './pages/ConsolePage';
-import { EmbedPage } from './pages/Embed';
 import './App.scss';
 
 function App() {
   return (
-    <Router>
-      <div data-component="App">
-        <nav>
-          <Link to="/">Console</Link>
-          <Link to="/embed">Embed</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<ConsolePage />} />
-          <Route path="/embed" element={<EmbedPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div data-component="App">
+      <ConsolePage />
+    </div>
   );
 }
 
